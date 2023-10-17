31c189856d7e618bdd692210af6ee7e9191b968b
Jacqueline Dobreva <Jacqueline.Dobreva@gmail.com>
2023-10-17 16:56:14 +0100

    Added stray files

---
+++ src/main/kotlin/com/jds/aes/AesApplication.kt
@@ -0,0 +1,11 @@
+package com.jds.aes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AesApplication
+
+fun main(args: Array<String>) {
+    runApplication<AesApplication>(*args)
+}
