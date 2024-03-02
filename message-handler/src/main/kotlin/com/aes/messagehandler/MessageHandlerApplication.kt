abd3c80617cee80576c301b60fbfa1bdfb966abe
Jacqueline Dobreva-Skevington <jacqueline.dobrevaskevington@t4connex.com>
2024-03-02 14:55:46 +0000

    Renamed to match module rename

--- message-handler/src/main/kotlin/com/aes/messagehandler/MessageHandlerApplication.kt
+++ message-handler/src/main/kotlin/com/aes/messagehandler/MessageHandlerApplication.kt
@@ -10,9 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @EnableJpaRepositories(basePackages = ["com.aes"])
 @EntityScan(basePackages = ["com.aes"])
 @ComponentScan(basePackages = ["com.aes"])
-class QaChatbotServiceApplication
+class MessageHandlerApplication
 
 fun main(args: Array<String>) {
-    runApplication<QaChatbotServiceApplication>(*args)
+    runApplication<MessageHandlerApplication>(*args)
 }
 
